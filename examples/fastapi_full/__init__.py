d76c4e9aeabac8b04b101534277fda333598133a
Eli <eli.serra173@gmail.com>
2026-07-02 07:51:05 +0200

    fix: align version to 1.0.1 in fastapi_full and fastapi_minimal examples

--- examples/fastapi_full/__init__.py
+++ examples/fastapi_full/__init__.py
@@ -1,5 +1,5 @@
 """fastapi_full fastapi app"""
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 __all__ = ["__version__"]
--- examples/fastapi_minimal/__init__.py
+++ examples/fastapi_minimal/__init__.py
@@ -1,5 +1,5 @@
 """Minimal fastapi-m8 consumer service example."""
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 __all__ = ["__version__"]
